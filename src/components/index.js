bd13ddf0100bc0f27acfd0be8b6eb6bab82c8a6a
fregnani123 <fabianofregnani@hotmail.com>
2022-12-13 16:15:56 -0300

    atualização do projeto contact funcionando.

--- src/components/index.js
+++ src/components/index.js
@@ -11,26 +11,30 @@ import logo from "../components/img/logo.png";
 function Header (){
 
     const [active, setMode] = useState (false);
-    const toggleMenu = () => {setMode(!active);}
-    
+    const toggleMenu = () => {setMode(!active)};
+
+    const [contact, setClass] = useState (false);
+    const toggleContact = () => {setClass(!contact);}
+
     return (
         <header className="header">
             <img className="logo" src={logo}></img>
             <nav className={active? "nav active" : "nav"}>
-                <button onClick={toggleMenu} className="btn-mobile">Menu <div className="hamburger"></div></button>
+
+          <button onClick={toggleMenu} className="btn-mobile">Menu <div className="hamburger"></div></button>
             <ul className="menu">
             <li><Link className="a">Home</Link></li>
             <li><Link className="a">About</Link></li>
             <li><Link className="a">Portfolio</Link></li>
-            <li><Link className="a">Contact</Link></li>
-            <ul className="social">
+            <li><Link onClick={toggleContact} className="a">contact</Link></li>
+            </ul>
+            </nav>
+            <ul className= {contact? "social contact":"social"}>
                 <li><a href="https://www.linkedin.com/in/fabiano-fregnani-8984051a7/" target="_blank"><img src={linkdin}/></a></li> 
                 <li><a href="https://github.com/fregnani123" target="_blank"><img src={git}/></a></li> 
                 <li><a href="https://www.facebook.com/fabiano.fregnani" target="_blank"><img src={face}/></a></li> 
                 <li><a href="https://www.instagram.com/fabiano_fregnani/" target="_blank"><img src={insta}/></a></li> 
             </ul>
-            </ul>
-            </nav>
         </header>
     )
 }
