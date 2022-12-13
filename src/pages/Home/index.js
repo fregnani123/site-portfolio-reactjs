a0646317ffdc17d16d453d49754b8e8c48ab3693
fregnani123 <fabianofregnani@hotmail.com>
2022-12-12 23:01:53 -0300

    Letras personalizadas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,13 +3,15 @@ import Header from "../../components";
 import bgVideo from "../Home/bgVideo.m4v"
 import "../Home/home.css"
 
+
 function Home() {
     return (
-        <div>
+        <div className="contents">
             <video className="bgVideo" autoPlay loop muted>
                 <source src={bgVideo} type="video/mp4" />
             </video>
             <Header />
+            <h1 className="dev">Front-End Developer</h1>
         </div>
     )
 
