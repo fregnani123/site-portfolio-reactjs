63e49180f040e12f9a8617a5ddec13fd3a3027e8
fregnani123 <fabianofregnani@hotmail.com>
2022-12-20 22:48:08 -0300

    Inicio pagina Portfolio

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,14 +1,32 @@
 import React from "react";
 import Header from "../../components";
 import "../Portfolio/portfolio.css";
+import Footer from "../../components/footer";
+import imgPortflio from "../Portfolio/Portfolio.png";
+
 
 function Portfolio(){
     return(
-        <div>
-            <Header/>
-            <h1>
-        Bem vindo - Portfolio - <br/> Página em contrução!</h1>
-            
+        <div className="contentPortfolio">
+          <div>
+          <Header/>
+<div className="quadroPortfolio">
+
+    <div className="quadro1">
+        <h1><img className="imgPortfolio" src={imgPortflio}/></h1>
+    </div>
+
+ <div>
+    <ul className="quadro2">
+        <li className="quadroLi">portfolio</li>
+        <li className="quadroLi">portfolio</li>
+        <li className="quadroLi">portfolio</li>
+        
+    </ul>
+</div>
+</div>
+        </div> 
+          <Footer/>
         </div>
     )
 }
