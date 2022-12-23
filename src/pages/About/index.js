bb12371511b9dd25217644cb40dc4e1f4eecdd62
fregnani123 <fabianofregnani@hotmail.com>
2022-12-23 01:08:28 -0300

    atualizando projeto

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,21 +1,27 @@
 import React from "react";
 import Header from "../../components";
 import "../About/about.css";
-import fab1 from "../About/img/fab1.png";
+import fab1 from "../About/img/fabiano.png";
+import imgAbout from "../About/img/about.png";
+import fundo from "../About/img/fundoNovo.png";
+
+
 
 function About() {
     return (
-        <div>
-            <Header/>
+        <div className="principal">
+        <Header/>
+        
        <div className="contentsAbout">
-        <h1>About</h1>
          <ul className="ulAbout">
-            <li className="lifab1">
-                <img className="fab1" src={fab1}/>
+           <li className="liAbout">
+                <img className="ImgAbout" src={imgAbout}/>
             </li>
-
-            <li className="lifab2">
-                <img className="fab2" src={fab1}/></li>
+            <li className="lifabi">
+              
+                <img className="fabi" src={fundo}/>
+            </li>
+            
          </ul>
        </div>
 
