4a5a33c31304578bb5fe04de09c30b11b682cbc7
fregnani123 <fabianofregnani@hotmail.com>
2022-12-23 23:39:56 -0300

    atualizar about

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,28 +1,30 @@
 import React from "react";
 import Header from "../../components";
 import "../About/about.css";
-import fab1 from "../About/img/fabiano.png";
-import imgAbout from "../About/img/about.png";
-import fundo from "../About/img/fundoNovo.png";
+import fundo from "../About/img/fabiano.png";
+import engrenagens from "../About/img/Engrenagens.m4v";
+import imgAbout from "../About/img/about.jpg";
+import gif from "../About/img/gifs.gif";
+
 
 
 
 function About() {
     return (
         <div className="principal">
-        <Header/>
-        
+        <video className="engrenagens" src={engrenagens} autoPlay loop muted type="mp4"/>
        <div className="contentsAbout">
+       <Header/>
          <ul className="ulAbout">
            <li className="liAbout">
-                <img className="ImgAbout" src={imgAbout}/>
+                 <p className="pAbout"><img className="gif" src={gif}/><img className="imgAbout" src={imgAbout}/></p>
+                 <img className="fabi" src={fundo}/>
             </li>
-            <li className="lifabi">
-              
-                <img className="fabi" src={fundo}/>
+            <li className="lifabiano">
+                
             </li>
             
-         </ul>
+         </ul>a
        </div>
 
         </div>
